aff0e72be1f2092e58075102d2a99eb4a6f7bf1b
Lejo Varughese <Lejo.Varughese@gmail.com>
2022-03-11 15:13:51 -0600

    Lejo - test content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Adding new content.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
