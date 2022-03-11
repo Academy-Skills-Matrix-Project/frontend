08be2dcb685beed850cd4e58f499407e78937b8b
bemymuse <66797764+bemymuse@users.noreply.github.com>
2022-03-11 16:37:00 -0500

    Adding Learn React to App.js of test to pass

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New Academy Frontend
+          Learn React
+          New Academy Architect
         </a>
       </header>
     </div>
