28f0c0ea07a44dbca3af428ef1d62ed70fb9d1c1
jtamayagk1 <jamaya614@yahoo.com>
2022-03-28 16:56:59 -0400

    Create navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './Navbar.css';
+
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <nav className='navbar'>
+                <img className='logo' src='SWO_logo.png' />
+                <h2 className='welcome'>Welcome to the Skill Matrix</h2>
+                <div className='item-holder'>
+                    <a><img className='nav-item' src='MyAccount.png'/></a>
+                    <a><img className='nav-item' src='Search.png' /></a>
+                    <a><img className='nav-item' src='LogOut.png'/></a>
+                </div> 
+            </nav>
+            
+        );
+    }
+}
+
+export default Navbar;
