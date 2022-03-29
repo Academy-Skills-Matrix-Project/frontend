30eddf74835b8ef13a4db334349185e755ebb918
chandlerHall47 <86384101+chandlerHall47@users.noreply.github.com>
2022-03-29 14:26:40 -0400

    added tests for navbar and created other pages

--- src/Components/NavHeader.js
+++ src/Components/NavHeader.js
@@ -25,12 +25,12 @@ const NavHeader = () => {
         // </>
         <>
            <nav className='navbar navbar-light bg-light'>
-           <img className='me-auto' src='SWO_logo.png' width={250} />
+           <img className='me-auto' src='SWO_logo.png' width={250} alt = 'SWO_Logo'/>
                 <span className='mx-auto'>Welcome to the Skill Matrix</span>
                 <div className='ms-auto'>
-                    <a><img className='nav-item' src='MyAccount.png' width={50} /></a>
-                    <a><img className='nav-item' src='Search.png' width={50} /></a>
-                    <a><img className='nav-item' src='LogOut.png' width={50} /></a>
+                    <a><img className='nav-item' src='MyAccount.png' width={50} alt= 'MyAccount' /></a>
+                    <a><img className='nav-item' src='Search.png' width={50} alt= 'Search'/></a>
+                    <a><img className='nav-item' src='LogOut.png' width={50} alt= 'LogOut'/></a>
                 </div>
            </nav>
         </>
--- src/Components/NavHeader.test.js
+++ src/Components/NavHeader.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
-import Navbar from './Navbar';
+import NavHeader from './NavHeader';
 
-test('Navbar.js renders Navbar', () => {
-    render(<Navbar />);
+test('NavHeader.js renders NavHeader', () => {
+    render(<NavHeader />);
     const testNavbarText = screen.getByText(/Welcome to the Skill Matrix/i);
     expect(testNavbarText).toBeInTheDocument();
+});
+
+describe('NavHeader.js', () => {test('Logo must have src = "/SWO_logo.png" and alt = "SWO_Logo"', () => {
+    render(<NavHeader/>);
+    const logo = screen.getByAltText('SWO_Logo');
+    expect(logo).toHaveAttribute('src', 'SWO_logo.png');
+    expect(logo).toHaveAttribute('alt', 'SWO_Logo');
+});
+});
+
+describe('NavHeader.js', () => {test('Logo must have src = "/MyAccount.png" and alt = "MyAccount"', () => {
+    render(<NavHeader/>);
+    const logo = screen.getByAltText('MyAccount');
+    expect(logo).toHaveAttribute('src', 'MyAccount.png');
+    expect(logo).toHaveAttribute('alt', 'MyAccount');
+});
+});
+
+describe('NavHeader.js', () => {test('Logo must have src = "/Search.png" and alt = "Search"', () => {
+    render(<NavHeader/>);
+    const logo = screen.getByAltText('Search');
+    expect(logo).toHaveAttribute('src', 'Search.png');
+    expect(logo).toHaveAttribute('alt', 'Search');
+});
+});
+
+describe('NavHeader.js', () => {test('Logo must have src = "/LogOut.png" and alt = "LogOut"', () => {
+    render(<NavHeader/>);
+    const logo = screen.getByAltText('LogOut');
+    expect(logo).toHaveAttribute('src', 'LogOut.png');
+    expect(logo).toHaveAttribute('alt', 'LogOut');
+});
 });
