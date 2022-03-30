c792a93cb0a491a02f414aa28eeb0bb698fff2d1
Whitney Harper <whitneyharper1@aol.com>
2022-03-30 17:55:58 -0400

    Changed paragraph text back to original text. Completed Banner tests and cleanup Banner test file.

--- src/Components/Banner/Banner.test.js
+++ src/Components/Banner/Banner.test.js
@@ -1,21 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import Banner from './Banner';
 
-test('banner should have title text', () => {
+test('banner should have title text Skills Matrix Abstract', () => {
   render(<Banner/>);
   const banner = screen.getByText('Skills Matrix Abstract');
-
   expect(banner).toHaveTextContent('Skills Matrix Abstract');
 })
 
- /*test('banner should have a paragraph text', () => {
+test('banner should have paragraph text This is card placeholder text', () => {
    render(<Banner/>);
-  const bannerParagraph = screen.getByText(/lori utem  asdflkbasd as;oih asdioa apsdfpaj dfpjad paijsd fpj asdpfj aspij asdifj aoi;sjdf;oasdf aklsdfio ajdsf;l;dlfkjasdkfnmlkfn. as;kfb;ajsdflnasn, /i)
-   expect(bannerParagraph).toBeInTheDocument();
- })*/
+  const bannerParagraph = screen.getByTestId('banner-text')
+   expect(bannerParagraph).toHaveTextContent('lori utem asdflkbasd asoih asdioa apsdfpaj dfpjad paijsd fpj asdpfj aspij asdifj aoisjdfoasdf aklsdfio ajdsfldlfkjasdkfnmlkfn. askfbajsdflnasn');
+ });
 
- test('banner should have a paragraph text', () => {
-  render(<Banner/>);
-  const bannerParagraph = screen.getByText('lori utem  asdflkbasd asoih asdioa apsdfpaj dfpjad paijsd fpj asdpfj aspij asdifj aoisjdfoasdf aklsdfio ajdsfldlfkjasdkfnmlkfn. askfbajsdflnasn')
-  expect(bannerParagraph).toHaveTextContent('lori utem  asdflkbasd asoih asdioa apsdfpaj dfpjad paijsd fpj asdpfj aspij asdifj aoisjdfoasdf aklsdfio ajdsfldlfkjasdkfnmlkfn. askfbajsdflnasn');
-})
+ test('renders Banner onto page', () => {
+   render(<Banner/>);
+   const bannerTitle = screen.getByText('Skills Matrix Abstract');
+   const bannerText = screen.getByText('lori utem asdflkbasd asoih asdioa apsdfpaj dfpjad paijsd fpj asdpfj aspij asdifj aoisjdfoasdf aklsdfio ajdsfldlfkjasdkfnmlkfn. askfbajsdflnasn');
+   expect(bannerText).toBeInTheDocument();
+   expect(bannerTitle).toBeInTheDocument();
+ });
+
+ 
