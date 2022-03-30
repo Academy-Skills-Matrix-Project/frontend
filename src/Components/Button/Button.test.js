37471ec8c8461936cfeacafce49c0eb7403f28fd
Essien <idong.essien@comcast.net>
2022-03-30 17:33:03 -0400

    Add Tests for Button Component

---
+++ src/Components/Button/Button.test.js
@@ -0,0 +1,21 @@
+import { render, screen} from '@testing-library/react';
+import Button from './Button';
+
+// Test for Button Component Render
+test('Sign-in button should be visible to user', () => {
+    render(<Button/>);
+
+    // Get the button element
+    const button = screen.getByTestId('sign-in-button');
+
+    expect(button).toBeInTheDocument();
+})
+
+// Button should read "Sign-in"
+test(`Button should read 'Sign-in'`, () => {
+    render(<Button/>);
+
+    const buttonText = screen.getByText('Sign-in');
+
+    expect(buttonText).toBeInTheDocument();
+})
