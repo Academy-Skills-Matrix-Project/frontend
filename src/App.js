9addac48a29d3b87ff639c6206258f68762744cd
Whitney Harper <whitneyharper1@aol.com>
2022-03-31 16:05:49 -0400

    Deleted NavHeader files and removed logo file from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
 import Banner from './Components/Banner/Banner';
 import LandingForm from './Components/LandingForm/LandingForm';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Banner />
       <LandingForm />
     </div>
