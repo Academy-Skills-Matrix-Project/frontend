392160dee016229993b325beede0ad788f3229f3
Whitney Harper <whitneyharper1@aol.com>
2022-04-04 16:57:04 -0400

    Changed texted in Banner.js and Banner.test.js to Lorem. Fixed LandingForm.js code to make the form mobile friendly.

--- src/Components/Banner/Banner.test.js
+++ src/Components/Banner/Banner.test.js
@@ -10,13 +10,13 @@ test('banner should have title text Skills Matrix Abstract', () => {
 test('banner should have paragraph text This is card placeholder text', () => {
    render(<Banner/>);
   const bannerParagraph = screen.getByTestId('banner-text')
-   expect(bannerParagraph).toHaveTextContent('lori utem asdflkbasd asoih asdioa apsdfpaj dfpjad paijsd fpj asdpfj aspij asdifj aoisjdfoasdf aklsdfio ajdsfldlfkjasdkfnmlkfn. askfbajsdflnasn');
+   expect(bannerParagraph).toHaveTextContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
  });
 
  test('renders Banner onto page', () => {
    render(<Banner/>);
    const bannerTitle = screen.getByText('Skills Matrix Abstract');
-   const bannerText = screen.getByText('lori utem asdflkbasd asoih asdioa apsdfpaj dfpjad paijsd fpj asdpfj aspij asdifj aoisjdfoasdf aklsdfio ajdsfldlfkjasdkfnmlkfn. askfbajsdflnasn');
+   const bannerText = screen.getByText('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
    expect(bannerText).toBeInTheDocument();
    expect(bannerTitle).toBeInTheDocument();
  });
