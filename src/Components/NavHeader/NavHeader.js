31bdffd1e2c541997bb00575b3f2af30f646ae4c
jtamayagk1 <jamaya614@yahoo.com>
2022-04-04 14:38:03 -0400

    made changes to nav items position

--- src/Components/NavHeader/NavHeader.js
+++ src/Components/NavHeader/NavHeader.js
@@ -17,26 +17,35 @@ class NavHeader extends Component {
 
     render() {
         return(
-        <>
-            <Navbar className='navbar navbar-light bg-light' data-testid = 'navbar'>
-                <div className="container-fluid">
-                    <div>
-                        <img className='me-auto' src='SWO_logo.png' width={250} alt = 'SWO_Logo'/>
-                    </div>
-                    <div className='mx-auto'>
-                        {!this.state.isLogoutEnabled && <h3>Welcome to the Skill Matrix</h3>}
-                    </div>
-                    <div className='ms-auto mx-4 px-4' >
-                        {this.state.isMyAccountEnabled && <a className='text-black' href='/' style={{textDecoration:'none'}} ><img className='nav-item' 
-                            src='MyAccount.png' width={50} alt= 'MyAccount' />My Account</a>}
-                        {this.state.isSearchEnabled && <a className='text-black' href='/' style={{textDecoration:'none'}} ><img className='nav-item' 
-                            src='Search.png'  width={50} alt= 'Search'/>Search</a>}
-                        {this.state.isLogoutEnabled && <a className='text-black' href='/' style={{textDecoration:'none'}} ><img className='nav-item' 
-                            src='LogOut.png' width={50} alt= 'LogOut'/>Logout</a>}
-                    </div>
-                </div>
+            <Navbar className='navbar' data-testid='navbar'>
+                <Container>
+                        <Col>
+                            <Navbar.Brand>
+                                <img src='SWO_logo.png' width={250} alt = 'SWO_Logo'/>
+                            </Navbar.Brand>
+                        </Col>
+                        <Col>
+                            {!this.state.isLogoutEnabled && <h1>Welcome to the Skill Matrix</h1>}
+                        </Col>
+                        <Col>
+                            <Nav>
+                                <Nav.Link>
+                                    {this.state.isMyAccountEnabled && <a className='text-black' href='/' style={{textDecoration:'none'}} ><img className='nav-item' 
+                                    src='MyAccount.png' width={50} alt= 'MyAccount' />My Account</a>}
+                                </Nav.Link>
+                                <Nav.Link>
+                                    {this.state.isSearchEnabled && <a className='text-black' href='/' style={{textDecoration:'none'}} ><img className='nav-item' 
+                                    src='Search.png'  width={50} alt= 'Search'/>Search</a>}
+                                </Nav.Link>
+                                <Nav.Link>
+                                    {this.state.isLogoutEnabled && <a className='text-black' href='/' style={{textDecoration:'none'}} ><img className='nav-item' 
+                                    src='LogOut.png' width={50} alt= 'LogOut'/>Logout</a>}
+                                </Nav.Link>
+                            </Nav>
+                        </Col>
+                </Container>  
             </Navbar>
-        </>
+     
             );
         }
     }
