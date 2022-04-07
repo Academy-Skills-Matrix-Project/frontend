37d9b1f48da2949afe225b622fe99765b906b36d
chandlerHall47 <86384101+chandlerHall47@users.noreply.github.com>
2022-04-07 13:59:30 -0400

    added red asterisks for required fields and corrected spacing

--- src/Components/GeneralInfo/GeneralInfo.js
+++ src/Components/GeneralInfo/GeneralInfo.js
@@ -23,13 +23,13 @@ function GeneralInfo(){
                     <h3>Name</h3>
                     </Col>
                     <Form.Group as={Col} controlId="formGridFirst" className="text-start" xs={6} sm={6} md={4}>
-                        <Form.Label data-testid= 'fName-label'>First Name*</Form.Label>
+                        <Form.Label data-testid= 'fName-label' className= 'redAsterisks'>First Name</Form.Label>
                     <Form.Control type="first" placeholder="Enter First Name" />
                     </Form.Group>
 
 
                     <Form.Group as={Col} controlId="formGridLast" className="text-start" xs={6} sm={6} md={4}>
-                    <Form.Label data-testid="lName-label">Last Name</Form.Label>
+                    <Form.Label data-testid="lName-label" className= 'redAsterisks'>Last Name</Form.Label>
                     <Form.Control type="last" placeholder="Enter Last Name" />
                     </Form.Group>   
                 </Row>
@@ -38,27 +38,27 @@ function GeneralInfo(){
                     <Col xs={8} sm={{span:8, order:5}} md={{span:4, order:0}}>
                     </Col>
                     <Form.Group as={Col} controlId="formGridJobT" className="text-start" xs={6} sm={{span:6, order: 1}} md={{span:4, order:1}}>
-                        <Form.Label data-testid="jTitle-label">Job Title</Form.Label>
+                        <Form.Label data-testid="jTitle-label" className= 'redAsterisks'>Job Title</Form.Label>
                     <Form.Control type="job" placeholder="Enter Job Title" />
                     </Form.Group>
 
 
                     <Form.Group as={Col} controlId="formGridDepartment" className="text-start" xs={6} sm={{span:6, order: 2}} md={{span:4, order:2}}>
-                    <Form.Label data-testid="dTitle-label">Department</Form.Label>
+                    <Form.Label data-testid="dTitle-label" className= 'redAsterisks'>Department</Form.Label>
                     <Form.Control type="department" placeholder="Enter Department" />
                     </Form.Group> 
 
                     <Col xs={12} sm={{span:12, order: 0}} md={{span:4, order:3}}>
                     <h3>Job</h3>
                     </Col>
-                    <Form.Group as={Col} controlId="formGridTeam" className="text-start" xs={6} sm={{span:6, order:3}} md={{span:4, order:4}}>
-                        <Form.Label data-testid="tTitle-label">Team</Form.Label>
+                    <Form.Group as={Col} controlId="formGridTeam" className="text-start mt-3" xs={6} sm={{span:6, order:3}} md={{span:4, order:4}}>
+                        <Form.Label data-testid="tTitle-label"className= 'redAsterisks'>Team</Form.Label>
                     <Form.Control type="team" placeholder="Enter Team Name" />
                     </Form.Group>
 
 
-                    <Form.Group as={Col} controlId="formGridLast" className="text-start" xs={6} sm={{span:6, order:4}} md={{span:4, order:5}}>
-                    <Form.Label data-testid="lTitle-label">Location/Time Zone</Form.Label>
+                    <Form.Group as={Col} controlId="formGridLast" className="text-start mt-3" xs={6} sm={{span:6, order:4}} md={{span:4, order:5}}>
+                    <Form.Label data-testid="lTitle-label" className= 'redAsterisks'>Location/Time Zone</Form.Label>
                     <Form.Select>
                         <option></option>
                         <option value="1">One</option>
@@ -74,13 +74,13 @@ function GeneralInfo(){
                     <h3>Contact <br></br> Information</h3>
                     </Col>
                     <Form.Group as={Col} controlId="formGridEmail" className="text-start" xs={6} sm={6} md={4}>
-                        <Form.Label data-testid='email-Label'>Email</Form.Label>
+                        <Form.Label data-testid='email-Label' className= 'redAsterisks'>Email</Form.Label>
                     <Form.Control type="email" placeholder="@softwareone.com" />
                     </Form.Group>
 
 
                     <Form.Group as={Col} controlId="formGridMobile" className="text-start" xs={6} sm={6} md={4}>
-                    <Form.Label data-testid= 'mNumber-label'>Mobile Number</Form.Label>
+                    <Form.Label data-testid= 'mNumber-label' >Mobile Number</Form.Label>
                     <Form.Control type="mobile" placeholder="Enter Phone #" />
                     </Form.Group>   
                 </Row>
@@ -99,7 +99,7 @@ function GeneralInfo(){
             </Container>
             <Container>
                 <Row>
-                    <Col>
+                    <Col className= 'mb-3'>
                         <Button title='Continue' data-testid="general-button"/>
                     </Col>
                 </Row>
