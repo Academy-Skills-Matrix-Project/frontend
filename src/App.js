a43ce8870771ac14fcf97ad5e2f6937845e3c879
Whitney Harper <whitneyharper1@aol.com>
2022-04-14 13:08:28 -0400

    Corrected failing tests. Needed to wrap MemoryRouter around components in each test.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 //import NavHeader from './Components/NavHeader/NavHeader';
 import GeneralInfo from './Pages/GeneralInfo';
 import SkillsPage from './Pages/SkillsPage';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import SearchPage from './Pages/SearchPage';
 
 
@@ -13,9 +13,6 @@ function App() {
   return (
     <div className='App'>
       <Router>
-          
-          {/* applies conditions to the props that will be passed to the navbar */}
-          {/* <NavHeader isLogoutEnabled={false} isSearchEnabled={false} isMyAccountEnabled={false}/> */}
           <main>
           <Switch>
             <Route path='/generalinfo'>
