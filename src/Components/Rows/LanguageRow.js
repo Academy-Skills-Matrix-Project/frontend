1fa453ac019d491685bee558e43dbafd0b1e1ba0
chandlerHall47 <86384101+chandlerHall47@users.noreply.github.com>
2022-04-19 12:05:18 -0400

    Created language row file

---
+++ src/Components/Rows/LanguageRow.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Row, Col} from 'react-bootstrap';
+import Rater from '../Rating/Rating';
+// import CategoryDropdown from '../DropdownButton/CategoryDropdown';
+import './SkillRow.css';
+import "react-widgets/styles.css";
+import ComboBox from 'react-widgets/Combobox';
+
+export default function LanguageRow(props) {
+    const {onRemove} = props;
+    return (
+        <Container>
+                    {/* Renders image for red 'X' button in top left of item  */}
+                    <button className='position-absolute top-0 start-0 translate-middle bg-transparent border-0' >
+                        <img  
+                            src="Cancel.png"
+                            width={30}
+                            alt='delete row'
+                            data-testid='delete-button'
+                            onClick={() => onRemove()}
+                            />
+                    </button>
+
+                <Row>
+                    <Col lg={3} md={12} xs={12} className='justify-content-center align-items-center' >
+                        <h4 className='px-3'>I am proficient in...</h4>
+                    </Col>
+                    <Col lg={6} xs={6} className='d-flex mx-auto justify-content-center align-items-center'>
+                        <ComboBox 
+                        className='textfield w-100 mx-1 text-start' 
+                        data-testid='skill-text-input'
+                        hideEmptyPopup 
+                        placeholder='Select skill' 
+                        data={['Communication','App Development', 'Public Speaking', 'Java', 'Python', 'asp','C++', 'a','b','c','5','1']}
+                        />
+                        {/* <Form.Control className="textfield form-control border border-2 w-75 m-2" type="input" data-testid='skill-text-input'/> */}
+                    </Col>
+                    {/* Renders star rating item */}
+                    <Col lg={3} md={12} xs={12}>
+                        <Rater/>
+                    </Col>       
+                </Row>
+        </Container>
+    );
+
+}
--- src/Pages/LanguagesPage.js
+++ src/Pages/LanguagesPage.js
@@ -2,26 +2,24 @@ import React from 'react';
 import AppButton from '../Components/Button/Button';
 import NavHeader from '../Components/NavHeader/NavHeader';
 import { Container } from 'react-bootstrap';
+import LanguageRow from '../Components/Rows/LanguageRow';
 
 
-class LanguagesPage extends React.Component {
+export default function LanguagesPage() {
 
 
-    
-    render() {
         return (
             <>
                 <NavHeader isLogoutEnabled={true} isSearchEnabled={false} isMyAccountEnabled={false} />
                 <div>
                     <h1>
                     <Container>
-                        <h1>LanguagePage</h1>
+                        <LanguageRow/>
                         <AppButton title="Continue" page='LanguagePage'/>
                     </Container>
                     </h1>
                 </div>
             </>
         );
-    }
+    
 }
-export default LanguagesPage;
