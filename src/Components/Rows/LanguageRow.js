9fccd509d12f558793855f3ad5c0d992224777ea
chandlerHall47 <86384101+chandlerHall47@users.noreply.github.com>
2022-04-19 12:48:03 -0400

    Created non-functional language page

--- src/Components/Rows/LanguageRow.js
+++ src/Components/Rows/LanguageRow.js
@@ -9,25 +9,24 @@ import ComboBox from 'react-widgets/Combobox';
 export default function LanguageRow(props) {
     const {onRemove} = props;
     return (
-        <Container>
+        <Container fluid className='position-relative mt-4 bg-light border rounded shadow-sm ' data-testid='language-row-container'>
                     {/* Renders image for red 'X' button in top left of item  */}
-                    <button className='position-absolute top-0 start-0 translate-middle bg-transparent border-0' >
-                        <img  
+                <button className= ' position-absolute top-0 start-0 translate-middle bg-transparent border-0 '>
+                <img  
                             src="Cancel.png"
                             width={30}
                             alt='delete row'
                             data-testid='delete-button'
                             onClick={() => onRemove()}
                             />
-                    </button>
-
-                <Row>
+                </button>
+                <Row data-testid='skill-row' className='py-1 text-center justify-content-center w-100'>
                     <Col lg={3} md={12} xs={12} className='justify-content-center align-items-center' >
                         <h4 className='px-3'>I am proficient in...</h4>
                     </Col>
                     <Col lg={6} xs={6} className='d-flex mx-auto justify-content-center align-items-center'>
                         <ComboBox 
-                        className='textfield w-100 mx-1 text-start' 
+                        className='textfield w-50 mx-1 text-start' 
                         data-testid='skill-text-input'
                         hideEmptyPopup 
                         placeholder='Select skill' 
