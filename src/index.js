8744f292c713501de8c2e2f43732a181b1e8739f
chandlerHall47 <86384101+chandlerHall47@users.noreply.github.com>
2022-05-04 14:53:19 -0400

    uncommented Router in app.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-   <React.StrictMode>
-    <App />
-   </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
